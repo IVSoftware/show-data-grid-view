694cb108ce431525dd5b18e1da69c7c87809f869
Tom Gregor <tgregor@ivsoftware.com>
2023-11-23 12:10:00 -0700

    Add project files.

---
+++ show-data-grid-view/ChooseDepPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace show_data_grid_view
+{
+    public partial class ChooseDepPage : Page
+    {
+        public ChooseDepPage()
+        {
+            base.DataContext = new ChooseDepPageModel();
+            InitializeComponent();
+
+            Loaded += async (sender, e) =>
+            {
+                // Turn ON for minimal test
+                StaffDGV.AutoGenerateColumns = true;
+                StaffDGV.DataSource = DataContext.Recordset;
+                StaffDGV.Font = new Font("Segoe UI", 16 );
+                StaffDGV.ColumnHeadersHeight = 60;
+                StaffDGV.RowTemplate.Height = 60;
+                StaffDGV.AllowUserToAddRows = false;
+                StaffDGV.Columns[nameof(Record.Index)].HeaderText = string.Empty;
+                StaffDGV.Columns[nameof(Record.Index)].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                StaffDGV.Columns[nameof(Record.Index)].Width = 60;
+                DataContext.Recordset.Add(new Record { Index = 1, Description = "Record A" });
+                DataContext.Recordset.Add(new Record { Index = 2, Description = "Record B" });
+                DataContext.Recordset.Add(new Record { Index = 3, Description = "Record C" });
+                await Task.Delay(100);
+                StaffDGV.CurrentCell = null;
+            };
+        }
+        ChooseDepPageModel DataContext => (ChooseDepPageModel)base.DataContext;
+        private void StaffDGV_DoubleClick(object sender, EventArgs e) { }
+        private void StaffDGV_SelectionChanged(object sender, EventArgs e) { }
+    }
+    class ChooseDepPageModel
+    {
+        public BindingList<Record> Recordset { get; } = new BindingList<Record>();
+    }
+    class Record
+    {
+        public int Index { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ show-data-grid-view/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace show_data_grid_view
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var frame = new Frame();
+            FrameApp.SetCurrentMainFrame(frame);
+            FrameApp.SetCurrentTopFrame(frame);
+            FrameApp.FrameTop.Navigate(new ChooseDepPage());
+
+            Content = frame; // << Check that content is being set "somewhere"
+        }
+    }
+    class FrameApp
+    {
+        public static Frame FrameMain { get; set; }
+        public static Frame FrameTop { get; set; }
+        public static void SetCurrentMainFrame(Frame frame) => FrameMain = frame;
+        public static void SetCurrentTopFrame(Frame frame) => FrameTop = frame;
+    }
+}
